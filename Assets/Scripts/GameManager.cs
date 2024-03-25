1b87083b8675f08eeb3bd7778495b6f409d65fbc
elio-1 <elioraph38@gmail.com>
2024-03-25 14:19:53 +0100

    fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-UnityEngine.SceneManagement
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
