3222c723643638e082dd2ef09b7a9d57824cbfca
elio-1 <elioraph38@gmail.com>
2024-03-26 13:49:24 +0100

    inventory

---
+++ Assets/Scripts/test/DebugLogBtn.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugLogBtn : MonoBehaviour
+{
+    public void DebugLogButton()
+    {
+        Debug.Log("This button is well pressed");
+    }
+}
